4af5327eb56021499499dcb9d7d46e739daade1d
jerryspann <jerryspann0@gmail.com>
2025-03-13 14:29:56 -0700

    added delete route and delete button

--- controllers/applications.js
+++ controllers/applications.js
@@ -34,19 +34,38 @@ router.post('/', async (req, res) => {
 // GET /users/:userId/applications/:applicationId
 router.get('/:applicationId', async (req, res) => {
     try {
-      // Look up the user from req.session
-      const currentUser = await User.findById(req.session.user._id);
-      // Find the application by the applicationId supplied from req.params
-      const application = currentUser.applications.id(req.params.applicationId);
-      // Render the show view, passing the application data in the context object
-      res.render('applications/show.ejs', {
-        application: application,
-      });
+        // Look up the user from req.session
+        const currentUser = await User.findById(req.session.user._id);
+        // Find the application by the applicationId supplied from req.params
+        const application = currentUser.applications.id(req.params.applicationId);
+        // Render the show view, passing the application data in the context object
+        res.render('applications/show.ejs', {
+            application: application,
+        });
     } catch (error) {
-      // If any errors, log them and redirect back home
-      console.log(error);
-      res.redirect('/');
+        // If any errors, log them and redirect back home
+        console.log(error);
+        res.redirect('/');
     }
-  });
+});
+
+// DELETE /users/:userId/applications/:applicationsId
+router.delete('/:applicationId', async (req, res) => {
+    try {
+        // Look up the user from req.session
+        const currentUser = await User.findById(req.session.user._id);
+        // Use the Mongoose .deleteOne() method to delete
+        // an application using the id supplied from req.params
+        currentUser.applications.id(req.params.applicationId).deleteOne();
+        // Save changes to the user
+        await currentUser.save();
+        // Redirect back to the applications index view
+        res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+        // If any errors, log them and redirect back home
+        console.log(error);
+        res.redirect('/');
+    }
+});
 
 module.exports = router;
